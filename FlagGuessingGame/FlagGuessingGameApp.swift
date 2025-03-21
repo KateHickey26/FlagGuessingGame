1383e2324e06a0260bf17ef82b555d93f4987c3f
KateHickey26 <kate.hickey26@gmail.com>
2025-03-21 16:20:21 +1100

    Initial Commit

---
+++ FlagGuessingGame/FlagGuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessingGameApp.swift
+//  FlagGuessingGame
+//
+//  Created by Kate Hickey on 21/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlagGuessingGameTests/FlagGuessingGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessingGameTests.swift
+//  FlagGuessingGameTests
+//
+//  Created by Kate Hickey on 21/03/2025.
+//
+
+import Testing
+@testable import FlagGuessingGame
+
+struct FlagGuessingGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
